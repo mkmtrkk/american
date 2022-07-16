341d139acc1318ccd00382a83fe393b173e7587e
Rika <mkmtrkk26@gmail.com>
2022-07-16 18:21:57 +0900

    wordpres編集中

--- wp-content/themes/american_village/footer.php
+++ wp-content/themes/american_village/footer.php
@@ -3,7 +3,7 @@
 </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="./american/wp-content/themes/american_village/script.js"></script>
+<script src="./wp-content/themes/american_village/script.js"></script>
 <?php wp_footer(); ?>
 </body>
 </html>
